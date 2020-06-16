033ce1c488e428b6fa627dbd405200eab678ef72
imlei <huanglei@live.com>
2020-06-16 11:00:25 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,2 +1,12 @@
 // I am learning the golang, and this one is help me to improve my work profermanace.
 package main
+import (
+    "database/sql"
+    "io"
+    "net/http"
+    "strings"
+    "text/template"
+    "time"
+    "github.com/ziutek/mymysql/godrv"
+   
+)
